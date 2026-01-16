2840366e7a77c8237f4c2e011c0a21da2236ee7f
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-17 03:59:17 +0700

    Add `AppThemeBuilder` for dynamic theme generation
    - Implement `AppThemeBuilder` to create `ThemeData` based on `AppThemeMode`.
    - Enable Material 3 design system and configure the `scaffoldBackgroundColor` and `colorScheme` seed based on the provided theme mode.
    - Integrate `GoogleFonts.poppinsTextTheme()` as the default text theme.
    - Support dynamic color configuration via the custom `AppThemeMode` enum.

---
+++ lib/core/themes/theme_builder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart' show ThemeData;
+import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
+import 'package:reminder/core/themes/theme_mode_enum.dart'
+    show AppThemeMode, AppThemeModeTools;
+
+class AppThemeBuilder {
+  const AppThemeBuilder();
+
+  static ThemeData from(AppThemeMode mode) => ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: mode.color.background.value,
+    colorScheme: .fromSeed(seedColor: mode.color.primary.value),
+    textTheme: GoogleFonts.poppinsTextTheme(),
+  );
+}
