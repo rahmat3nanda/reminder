7ac9e4faa7816c474b5d8bef24ba820723fdc576
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-17 03:21:52 +0700

    Implement `AppBlocObserver` for centralized BLoC logging
    - Create `AppBlocObserver` extending `BlocObserver` to monitor BLoC events, transitions, and errors.
    - Integrate `AppLogger` to log information for `onEvent` and `onTransition` calls.
    - Implement fatal error logging in `onError` to capture BLoC-level exceptions and stack traces.

---
+++ lib/core/bloc_observer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_bloc/flutter_bloc.dart'
+    show Bloc, BlocBase, BlocObserver, Transition;
+import 'package:reminder/core/logger.dart' show AppLogger;
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    super.onEvent(bloc, event);
+    AppLogger.i.info('${bloc.runtimeType} $event');
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    AppLogger.i.info(transition);
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    AppLogger.i.fatal('${bloc.runtimeType} $error');
+    super.onError(bloc, error, stackTrace);
+  }
+}
