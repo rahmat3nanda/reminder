216ea0d5523e4e2017640c1ad7dcc515a16bc6cc
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-17 18:11:50 +0700

    Update `ThemeBuilder` to refine application themes
    - Configure `AppBarTheme` with Inter font for titles and theme-specific colors for icons.
    - Set `brightness` in `ColorScheme.fromSeed` based on the current theme mode.
    - Update imports to include `AppBarTheme` and `IconThemeData`.

--- lib/cores/themes/theme_builder.dart
+++ lib/cores/themes/theme_builder.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart' show ThemeData;
+import 'package:flutter/material.dart'
+    show AppBarTheme, IconThemeData, ThemeData;
 import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
 import 'package:reminder/cores/themes/domain/theme_mode_enum.dart'
     show AppThemeMode, AppThemeModeTools;
@@ -8,8 +9,15 @@ class AppThemeBuilder {
 
   static ThemeData from(AppThemeMode mode) => ThemeData(
     useMaterial3: true,
+    appBarTheme: AppBarTheme(
+      titleTextStyle: GoogleFonts.inter(),
+      iconTheme: IconThemeData(color: mode.color.text.value),
+    ),
     scaffoldBackgroundColor: mode.color.background.value,
-    colorScheme: .fromSeed(seedColor: mode.color.primary.value),
+    colorScheme: .fromSeed(
+      seedColor: mode.color.primary.value,
+      brightness: mode.brightness,
+    ),
     textTheme: GoogleFonts.poppinsTextTheme(),
   );
 }
